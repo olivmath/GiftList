cd555cdf630797b164773b4328ea1825e0a67957
Lucas Oliveira <olivmath@protonmail.com>
2024-09-27 01:33:11 -0300

    update: refactor

--- serverjs/src/index.js
+++ serverjs/src/index.js
@@ -1,29 +1,29 @@
-const express = require('express');
-const cors = require('cors');
+const express = require("express");
+const cors = require("cors");
 const port = 1225;
 const app = express();
-const verifyProof = require("./utils")
+const verifyProof = require("./utils");
 
 app.use(express.json());
 app.use(cors());
 
+const MERKLE_ROOT =
+  "0xe35e6e14fdf91ecc6adfb74856bcd8a2c22544bd10bded94f2a9fecc77cf630b";
 
-const MERKLE_ROOT = '0xe35e6e14fdf91ecc6adfb74856bcd8a2c22544bd10bded94f2a9fecc77cf630b';
+app.post("/vip", (req, res) => {
+  const { proof, leaf } = req.body;
 
-
-app.post('/vip', (req, res) => {
-    const { proof, leaf } = req.body;
-
-    if (verifyProof(proof, leaf, MERKLE_ROOT)) {
-        res.send("You are Vip ✅");
-    }
-    else {
-        res.status(401).send({
-            message: "You are not VIP 🚨"
-        });
-    }
+  if (verifyProof(proof, leaf, MERKLE_ROOT)) {
+    res.status(200).send({
+      message: "You are Vip ✅",
+    });
+  } else {
+    res.status(401).send({
+      message: "You are not VIP 🚨",
+    });
+  }
 });
 
 app.listen(port, () => {
-    console.log(`Listening on port ${port}!`);
+  console.log(`Listening on port ${port}!`);
 });
